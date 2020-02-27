2d1b6017724d99220858a55855150a6676bea959
jimmy <jim.dangquoc706@gmail.com>
2020-02-27 11:37:41 +0700

    fix demo6

--- demo6/js/scroll.js
+++ demo6/js/scroll.js
@@ -1,6 +1,6 @@
 var sidebar = document.getElementById("sidebar");
-
 var topBtn = document.getElementById("top-btn");
+var lastedScroll = window.scrollY;
 
 topBtn.addEventListener("click", function() {
   window.scroll({
@@ -11,9 +11,10 @@ topBtn.addEventListener("click", function() {
 
 window.addEventListener("scroll", function() {
   var distance = window.scrollY;
-
+  var isScrollDown = distance - lastedScroll > 0 ? true : false;
   if (distance > 245) {
     var top = distance - 245;
+
     var wHeight = window.innerHeight + window.scrollY;
     var bodyHeight = document.body.offsetHeight;
     if (!topBtn.classList.contains("block")) {
@@ -23,10 +24,11 @@ window.addEventListener("scroll", function() {
     if (wHeight >= bodyHeight) {
       sidebar.style.top = `${top -
         (wHeight - bodyHeight) -
-        (window.innerHeight > 600 ? 32 : 80)}px`;
+        (window.innerHeight > 600 ? 32 : 200)}px`;
     }
   } else {
     sidebar.style.top = "0";
     topBtn.classList.remove("block");
   }
+  lastedScroll = distance;
 });
