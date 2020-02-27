9ae826f0e264056d11e72c86b50fd1b8282464ea
jimmy <jim.dangquoc706@gmail.com>
2020-02-27 09:27:37 +0700

    demo4 complete

--- demo6/js/scroll.js
+++ demo6/js/scroll.js
@@ -11,21 +11,18 @@ topBtn.addEventListener("click", function() {
 
 window.addEventListener("scroll", function() {
   var distance = window.scrollY;
-  if (distance > 60) {
+
+  if (distance > 245) {
+    var top = distance - 245;
     if (!topBtn.classList.contains("block")) {
       topBtn.classList.add("block");
     }
-  } else {
-    topBtn.classList.remove("block");
-  }
-
-  if (distance > 245) {
-    let top = distance - 245;
     sidebar.style.top = `${top}px`;
     if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
       sidebar.style.top = `${top - 32}px`;
     }
   } else {
     sidebar.style.top = "0";
+    topBtn.classList.remove("block");
   }
 });
