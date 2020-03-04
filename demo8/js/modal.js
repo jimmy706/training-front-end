54cb6480e93029367288ae268c8a4306b6cb9908
jimmy <jim.dangquoc706@gmail.com>
2020-03-04 18:24:30 +0700

    done demo8 desktop header

--- demo8/js/modal.js
+++ demo8/js/modal.js
@@ -1,20 +1,35 @@
 var modalToggles = Array.from(document.getElementsByClassName("modal-toggle"));
 
+function closeModal(modalTarget) {
+  var content = modalTarget.querySelector(".modal-content");
+  content.classList.remove("on-open");
+  setTimeout(function() {
+    modalTarget.classList.remove("open");
+  }, 300);
+}
+
+function openModal(modalTarget) {
+  var content = modalTarget.querySelector(".modal-content");
+  modalTarget.classList.add("open");
+  content.classList.add("on-open");
+}
+
 modalToggles.forEach(toggle => {
   toggle.addEventListener("click", function() {
     var target = this.dataset["target"];
     if (target) {
       var modalTarget = document.querySelector(target);
       var closeBtn = modalTarget.querySelector(".close");
-      modalTarget.classList.add("open");
-
-      closeBtn.addEventListener("click", function() {
-        modalTarget.classList.remove("open");
-      });
+      openModal(modalTarget);
+      if (closeBtn) {
+        closeBtn.addEventListener("click", function() {
+          closeModal(modalTarget);
+        });
+      }
 
       window.onclick = function(event) {
         if (event.target == modalTarget) {
-          modalTarget.classList.remove("open");
+          closeModal(modalTarget);
         }
       };
     }
