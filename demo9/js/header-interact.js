ccd58e7cb4d7ce03a72f6cea10f70721ba431b94
jimmy <jim.dangquoc706@gmail.com>
2020-03-12 21:38:05 +0700

    demo8 update

---
+++ demo9/js/header-interact.js
@@ -0,0 +1,23 @@
+const searchToggle = document.getElementById("search-toggle");
+const cartToggle = document.getElementById("cart-toggle");
+
+searchToggle.addEventListener("click", e => {
+  e.preventDefault();
+  const searchForm = document.querySelector("#header-search-form");
+  if (!searchForm.classList.contains("on-open")) {
+    searchToggle.classList.add("open");
+    searchForm.classList.add("on-open");
+  } else {
+    searchToggle.classList.remove("open");
+    searchForm.classList.remove("on-open");
+  }
+});
+
+cartToggle.addEventListener("click", e => {
+  e.preventDefault();
+  if (!cartToggle.classList.contains("open")) {
+    cartToggle.classList.add("open");
+  } else {
+    cartToggle.classList.remove("open");
+  }
+});
